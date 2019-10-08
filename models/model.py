c1820c1c3bcd16dbc13fe456769c1a361bbc1873
= <smuralid@sfu.ca>
2019-10-07 17:33:06 -0700

    complete argparse

--- models/model.py
+++ models/model.py
@@ -7,15 +7,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model_prefix', default='./models/',help='model checkpoint directory')
 parser.add_argument('-c', '--conditional', action='store_true',help='conditional v/s unconditionali')
 
+args = parser.parse_args()
+
 
 strokes = numpy.load('../data/strokes.npy')
-stroke = strokes[0]
+print(strokes[0])
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.model = nn.LSTM()
-        self.
+        if not args.c:
+            self.lstm1 = nn.LSTM(3,256)
+        else:
+            raise
+        self.fc1 = nn.Linear(256,128)
+        self.fc2 = nn.Linear(128,3)
+        #TODO Incorporate mixture density model
+
+    def forward(x=None):
+        pass
+
 
 def generate_unconditionally(random_seed=1):
     # Input:
